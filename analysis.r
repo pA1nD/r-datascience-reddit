d715f2dd9c43e01c02df8efe7a502bc8d2e358ae
Bryan Giger <bryan@gigerbyt.es>
2017-11-25 12:02:49 +0100

    analysis read from clean_posts.csv, bugfix

--- analysis.r
+++ analysis.r
@@ -6,7 +6,9 @@ library("tm")
 #library("wordcloud")
 library("RColorBrewer")
 # main --------------------------------------------------------------------
-
+# Read clean data csv
+df = read_delim("data/news_2016_12.csv")
+df_clean = read_delim("data/clean_posts.csv", sep=",")
 
 # General Plotting --------------------------------------------------------
 
@@ -30,11 +32,11 @@ plotUps <- function(df){
   plot(dfUpsGr1$ups, type="l", col="red", ylab="log Upvotes > 1") # most posts only have 1 upvote, >0 fol LN scale
   
   # calculate upvotes>1 / total Upvotes
-  print(length(dfUpsGr1) / length(dfUps$ups))
+  print(length(dfUpsGr1$ups) / length(dfUps$ups) * 100 ) 
 }
-print("unclean data")
+print("% ups > 1 for unclean data")
 plotUps(df)
-print("clean data")
+print("% ups > 1 for clean data")
 plotUps(df_clean)
 # Influencer : Post language
 # can see that the removal of non-english posts have a positive effect on upvotes
@@ -48,4 +50,10 @@ plotUps(df_clean)
 
 binUps = function(df){
   maxInt= ceiling(max(df))
+  binList = seq(1, maxInt)
+  binUps = matrix(0, nrows=2, ncol=maxInt)
+  binUps[,1] = binList
+  for(i in bins){
+    
+  }
 }
