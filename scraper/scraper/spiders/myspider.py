a71dd21cb8c54af5b89b28c46355099c287e4753
Bryan Giger <bryan@gigerbyt.es>
2017-11-27 02:29:40 +0100

    Title scraper finished

--- scraper/scraper/spiders/myspider.py
+++ scraper/scraper/spiders/myspider.py
@@ -1,4 +1,6 @@
 import scrapy
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
 
 class MySpider(scrapy.Spider):
     name = "myspider"
@@ -9,4 +11,18 @@ class MySpider(scrapy.Spider):
             with open(filename, 'r') as f:
                 print("HIII*****************8")
                 self.start_urls = f.read().splitlines()
+        f = open("titles.txt","w")
+        f.close()
+        self.i = 0
         print(self.start_urls)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        body = response.body
+        #print(body)
+        title = ''.join(Selector(text=body).xpath('//title/text()').extract())
+        print(title)
+        title =  "".join([line.strip() for line in title.strip().splitlines()])
+        csvLine = response.url + "; " + title + "\n"
+        f = open("titles.txt","a");
+        f.write(csvLine)
